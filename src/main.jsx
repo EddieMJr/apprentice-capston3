02d441c4d17683fb69be1d46aa1417f065a89009
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-11-24 11:46:58 -0500

    setup file structure

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
