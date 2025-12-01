4af26de92be9fecdf739e1e23c80cc5509c567a8
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-12-01 10:02:24 -0500

    Pushing my changes- Eddie

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Dashboard from './pages/Dashboard.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
